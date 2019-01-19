4fad746feb1653164845850621aa23285cd88c87
CAPAU-111316 <you@example.com>
2019-01-19 18:19:38 -0300

    A funcionalidade de listagem de animais foi adicionado ao projeto

--- gestao/models.py
+++ gestao/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Animal(models.Model):
+
+      especialidade_choices = (
+            ('Abate', 'Abate'),
+            ('Reprodutor', 'Reprodutor')
+      )
+
+      nome = models.CharField(max_length=20)
+      cientifico = models.CharField(max_length=20, verbose_name='Nome Científico')
+      expectativa = models.CharField(max_length=10, verbose_name='Expectativa de Vida')
+      periodo = models.CharField(max_length=10, verbose_name='Periodo de Gestação')
+      peso = models.DecimalField(max_digits=6, decimal_places=2)
+      velocidade = models.IntegerField()
+      codigo = models.CharField(max_length=11, verbose_name='Código de Identificação')
+      raca = models.CharField(max_length=20, verbose_name='Raça')
+      objetivo = models.CharField(max_length=10, choices=especialidade_choices)
--- gestao/views.py
+++ gestao/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from . models import Animal
 
 def home(request):
     return render(request, 'home.html')
+
+def list_animais(request):
+      animais = Animal.objects.all()
+      return render(request, 'animal/list.html', {'animais':animais})
