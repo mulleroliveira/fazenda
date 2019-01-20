c97744875eed111ddcd595b45c31204999de43ed
CAPAU-111316 <you@example.com>
2019-01-20 13:19:05 -0300

    Implementação da classe Fazendeiro no projeto.

--- gestao/models.py
+++ gestao/models.py
@@ -9,3 +9,45 @@ class Animal(models.Model):
       tamanho = models.DecimalField(max_digits=5,decimal_places=2)
       expectativa = models.IntegerField(verbose_name='Expectativa de Vida')
       objetivo = models.CharField(max_length=10, choices=[('Abate','Abate'),('Reprodutor','Reprodutor')])
+
+class Fazendeiro(models.Model):
+
+      choices_estado = (
+            ('AC','Acre'),
+            ('ALA','lagoas'),
+            ('AP','Amapá'),
+            ('AMA','mazonas'),
+            ('BA','Bahia'),
+            ('CE','Ceará'),
+            ('DF','Distrito Federal'),
+            ('ES','Espírito Santo'),
+            ('GO','Goiás'),
+            ('MA','Maranhão'),
+            ('MT','Mato Grosso'),
+            ('MS','Mato Grosso do Sul'),
+            ('MG','Minas Gerais'),
+            ('PA','Pará'),
+            ('PB','Paraíba'),
+            ('PR','Paraná'),
+            ('PE','Pernambuco'),
+            ('PI','Piauí'),
+            ('RR','Roraima'),
+            ('RO','Rondônia'),
+            ('RJ','Rio de Janeiro'),
+            ('RN','Rio Grande do Norte'),
+            ('RS','Rio Grande do Sul'),
+            ('SC','Santa Catarina'),
+            ('SP','São Paulo'),
+            ('SE','Sergipe'),
+            ('TO','Tocantins'),
+      )
+
+      nome = models.CharField(max_length=20)
+      data_nasc = models.DateField()
+      cpf = models.CharField(max_length=11)
+      rg = models.CharField(max_length=9)
+      funcao = models.CharField(max_length=20)
+      peso = models.DecimalField(max_digits=5, decimal_places=2)
+      altura = models.DecimalField(max_digits=3, decimal_places=2)
+      cidade = models.CharField(max_length=20)
+      estado = models.CharField(max_length=2, choices=choices_estado)
