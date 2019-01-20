bf37aef2694cd45ff79209132fbc7dcbdbc6ecb6
CAPAU-111316 <you@example.com>
2019-01-19 22:57:32 -0300

    adicionado ao home, na tabela, os 3 últimos animais cadastrados.

--- gestao/views.py
+++ gestao/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 from . models import Animal
 
 def home(request):
-    return render(request, 'home.html')
+      lista = []
+      animais = Animal.objects.all()
+      for c in animais[::-1]:
+            if len(lista) == 3:
+                  break
+            elif c in lista:
+                  pass
+            else:
+                  lista.append(c)
+      return render(request, 'home.html', {'lista':lista})
 
 def list_animais(request):
       animais = Animal.objects.all()
